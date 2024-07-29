02f1d4e9dc7d8148dd89c456204c4471dfb54652
Oleg <100606637+OlegEco@users.noreply.github.com>
2024-07-29 16:04:41 +0200

    Add project files.

---
+++ ExtensionMethodsForWorkingWithAsyncOperations/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using ExtensionMethodsForWorkingWithAsyncOperations;
+try
+{
+    string url = "https://jsonplaceholder.typicode.com/posts";
+    string data = await FetchDataAsync(url).WithTimeout(2000);
+    Console.WriteLine(data);
+}
+catch (TimeoutException ex)
+{
+    Console.WriteLine($"Timeout error: {ex.Message}");
+}
+
+
+async Task<string> FetchDataAsync(string url)
+{
+    using (HttpClient client = new HttpClient())
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        string responseData = await response.Content.ReadAsStringAsync();
+        return responseData;
+    }
+}
---
+++ ExtensionMethodsForWorkingWithAsyncOperations/TaskExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsForWorkingWithAsyncOperations
+{
+    internal static class TaskExtensions
+    {
+        public static async Task<T> WithTimeout<T>(this Task<T> task, int timeout)
+        {
+            if (await Task.WhenAny(task, Task.Delay(timeout)) == task) // waiting completed Task or TImeOut
+            {
+                return await task;
+            }
+            else
+            {
+                throw new TimeoutException("The operation has timed out."); // if exception then TimeoutException
+            }
+        }
+    }
+}
